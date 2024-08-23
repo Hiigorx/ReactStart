622b19d8f807c876d0bba234072351c886a7b152
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-08-22 21:35:21 -0300

    Add files via upload

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+function PageNotFound() {
+    return ( 
+        <h1>
+            Página não encontrada
+        </h1>
+
+
+     );
+}
+
+export default PageNotFound;
---
+++ src/pages/Servicos.jsx
@@ -0,0 +1,11 @@
+function Servicos() {
+    return ( 
+        <h1>
+            Servicos
+        </h1>
+
+
+     );
+}
+
+export default Servicos;
